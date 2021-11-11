2639937b0c17940c3313f3ffbd3501bfc01dd62d
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-11-11 17:34:27 -0500

    EmpList blazor

--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -8,7 +8,7 @@ namespace EmployeeManagement.Models
 {
     public class Employee
     {
-        public int Id { get; set; }
+        public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
@@ -16,7 +16,6 @@ namespace EmployeeManagement.Models
         public Gender Gender { get; set; }
         public Department Department { get; set; }
         public string PhotoPath { get; set; }
-
-
+        public string Name { get; set; }
     }
 }
---
+++ EmployeeManagement.UI/Pages/EmployeeListBase.cs
@@ -0,0 +1,65 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.UI.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployees();
+            return base.OnInitializedAsync();
+        }
+        private void LoadEmployees()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "john@pragimtech.com",
+                DateOfBirth = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 1, DepartmentName = "HumanResources" },
+                PhotoPath = "images/John.jfif"
+            };
+            Employee e2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Ryan",
+                LastName = "North",
+                Email = "Ryan@pragimtech.com",
+                DateOfBirth = new DateTime(1981, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 2, DepartmentName = "HumanResourcesIT" },
+                PhotoPath = "images/Ryan.jfif"
+            };
+            Employee e3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Tyler",
+                LastName = "Branscom",
+                Email = "Tyler@pragimtech.com",
+                DateOfBirth = new DateTime(1989, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 3, DepartmentName = "Oyova" },
+                PhotoPath = "images/Tyler.jfif"
+            };
+            Employee e4 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Brandon",
+                LastName = "Miller",
+                Email = "Brandon@pragimtech.com",
+                DateOfBirth = new DateTime(1990, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 4, DepartmentName = "Terminated" },
+                PhotoPath = "images/Brandon.jpg"
+            };
+
+            Employees = new List<Employee> { e1, e2, e3, e4 };
+        }
+    }
+}
