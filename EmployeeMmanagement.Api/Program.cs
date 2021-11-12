069dabc93151d29f98fe22257b0ed99ed14717f7
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-11-12 12:17:02 -0500

    Setup the database and added DBContext.

--- EmployeeMmanagement.Api/Program.cs
+++ EmployeeMmanagement.Api/Program.cs
@@ -1,8 +1,12 @@
+using EmployeeMmanagement.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
