8d409310461f373831b36582a84e3317c11a9edc
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-11-13 13:28:06 -0500

    temp

--- EmployeeManagement.UI/Pages/EmployeeListBase.cs
+++ EmployeeManagement.UI/Pages/EmployeeListBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.UI.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace EmployeeManagement.UI.Pages
@@ -7,59 +8,12 @@ namespace EmployeeManagement.UI.Pages
     {
         public IEnumerable<Employee> Employees { get; set; }
 
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
-            await Task.Run(LoadEmployees);
-        }
-        private void LoadEmployees()
-        {
-            System.Threading.Thread.Sleep(3000);
-            Employee e1 = new Employee
-            {
-                EmployeeId = 1,
-                FirstName = "John",
-                LastName = "Hastings",
-                Email = "john@pragimtech.com",
-                DateOfBirth = new DateTime(1980, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 1,
-                PhotoPath = "images/John.jfif"
-            };
-            Employee e2 = new Employee
-            {
-                EmployeeId = 2,
-                FirstName = "Ryan",
-                LastName = "North",
-                Email = "Ryan@pragimtech.com",
-                DateOfBirth = new DateTime(1981, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 2,
-                PhotoPath = "images/Ryan.jfif"
-            };
-            Employee e3 = new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Tyler",
-                LastName = "Branscom",
-                Email = "Tyler@pragimtech.com",
-                DateOfBirth = new DateTime(1989, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 3,
-                PhotoPath = "images/Tyler.jfif"
-            };
-            Employee e4 = new Employee
-            {
-                EmployeeId = 1,
-                FirstName = "Brandon",
-                LastName = "Miller",
-                Email = "Brandon@pragimtech.com",
-                DateOfBirth = new DateTime(1990, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 4,
-                PhotoPath = "images/Brandon.jpg"
-            };
-
-            Employees = new List<Employee> { e1, e2, e3, e4 };
+           Employees = await EmployeeService.GetEmployees();
         }
     }
 }
---
+++ EmployeeManagement.UI/Services/EmployeeService.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeManagement.Models;
+using System.Net.Http.Json;
+
+namespace EmployeeManagement.UI.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await _httpClient.GetFromJsonAsync<Employee[]>("api/employees");
+        }
+
+        public Task<Employee> AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> DeleteEmployee(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeById(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> Search(string name, Gender? gender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
