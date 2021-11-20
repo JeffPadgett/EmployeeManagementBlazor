eaca5f1264edbf6f7deb590313eeba5ae9b3c98c
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-11-19 20:21:58 -0500

    Before Cleanup.

---
+++ StreamServices.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using StreamServices;
+using StreamServices.API;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace StreamServices.API
+{
+    public sealed class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+            builder.Services.AddAutoMapper(typeof(Startup).Assembly);
+        }
+    }
+
+}
